6d7f61f007bbf878b3aea33f0a261bfefd73fea5
Tran Son <sontd.hust@gmail.com>
2019-12-01 11:11:12 +0900

    Do not need to augment the transformation of the states before training.

--- dqn_2048/game/agent/quality.py
+++ dqn_2048/game/agent/quality.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from os import makedirs, path
 from typing import List, Tuple
 
-from numpy import array, argmax
+from numpy import array, argmax, ndarray
 from keras.layers import Dense
 from keras.models import Sequential
 from keras.optimizers import SGD
@@ -34,43 +34,36 @@ class Quality(BaseQuality):
         """
         super().__init__(gamma)
         self.output_size = output_size
-        self.model = self.__create_model(input_size, output_size)
-        self.model.compile(SGD(learning_rate), loss="mse")
+        self.__model = self.__create_model(input_size, output_size)
+        self.__model.compile(SGD(learning_rate), loss="mse")
+        self.__learning_model = None
 
     def learn(self, batch: List[Experience]):
-        augmented_batch: List[Experience] = []
-        for experience in batch:
-            state: State = experience.state
-            action: Action = experience.action
-            value = experience.value
-            augmented_batch.extend([
-                Experience(state, action, value),
-                Experience(state.rotate_left(), action.rotate_left(), value),
-                Experience(state.rotate_right(), action.rotate_right(), value),
-                Experience(state.turn(), action.turn(), value),
-                Experience(state.flip(), action.flip(), value),
-                Experience(state.flip().rotate_left(), action.flip().rotate_left(), value),
-                Experience(state.flip().rotate_right(), action.flip().rotate_right(), value),
-                Experience(state.flip().turn(), action.flip().turn(), value)
-            ])
-        state_list: List[State] = [experience.state for experience in augmented_batch]
+        state_list: List[State] = [experience.state for experience in batch]
         state_data = array([state.data for state in state_list])
-        action_data = self.model.predict(state_data)
-        for i, experience in enumerate(augmented_batch):
+        action_data = self.__model.predict(state_data)
+        for i, experience in enumerate(batch):
             action: Action = experience.action
             action_data[i][action.data] = experience.value
-        self.model.fit(state_data, action_data, verbose=0)
+        self.__model.fit(state_data, action_data, verbose=0)
 
     def copied(self, training_quality: Quality):
-        self.model.set_weights(training_quality.model.get_weights())
+        self.__model.set_weights(training_quality.weights)
 
     def save(self, dir_path: str):
         makedirs(dir_path, exist_ok=True)
         now = datetime.now().strftime("%Y%m%d_%H%M%S")
-        self.model.save_weights(path.join(dir_path, f"{now}.hdf5"))
+        self.__model.save_weights(path.join(dir_path, f"{now}.hdf5"))
+
+    @property
+    def weights(self) -> List[ndarray]:
+        """
+        # Returns the weights of the model.
+        """
+        return self.__model.get_weights()
 
     def _select_action(self, state: State) -> Tuple[Action, float]:
-        values = self.model.predict(array([state.data]))[0]
+        values = self.__model.predict(array([state.data]))[0]
         max_index = argmax(values)
         return (Action(Direction(max_index)), values[max_index])
 
